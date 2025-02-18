ec39b55e774436ebce9fefc24c9f20157e3525fb
Nitinsingh7643 <nitinsingh6207816283@gmail.com>
2025-02-18 13:21:41 +0530

    committed changes

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index2*.html", "./src/**/*.{js,jsx,ts,tsx}"], // Add more paths as needed
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
